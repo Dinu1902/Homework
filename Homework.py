ae6eb761c64ebdef0bd6325529250d7b72438619
Dinu1902 <dinu.bricicari@mail.ru>
2022-09-29 17:11:20 +0300

    file1 added

---
+++ Homework.py
@@ -0,0 +1,9 @@
+#Python = (3.10)
+#print("Aveti instalata versiunea " + str(Python))
+
+
+
+data=(input("Inserati data de astazi in formatul zz.ll.yyyy. : "))
+ora=(input('Inserati ora : '))
+minutele=(input("Inserati minutele : "))
+print("Astazi este data de : " + data, "si este ora ", + int(ora) ,"si ", + int(minutele) ,"minute")
